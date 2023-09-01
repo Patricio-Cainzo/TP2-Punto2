f6f40cc6018ff8961213d64d9688ba6174cd2b7d
pcain <pcain@MSI>
2023-09-01 18:03:13 -0300

    Agregar archivos de proyecto.

---
+++ TP2-Punto2/Persona.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Punto2
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad {get; set; }
+        public int Dni {  get; set; }
+        public char Sexo {  get; set; }
+        public decimal Peso { get; set; }
+        public decimal Altura {  get; set; }
+
+     
+        public Persona() { }
+        public Persona(string nombre,int edad,char sexo) 
+        {
+            Nombre= nombre;
+            Edad= edad;
+            Sexo = sexo;
+        }
+        public Persona(string nombre, int edad, int dni, char sexo, decimal peso, decimal altura)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Dni = dni;
+            Sexo = sexo;
+            Peso = peso;
+            Altura = altura;
+
+        }
+        public int IMC(decimal peso,decimal altura)
+        {
+            decimal Alturaenmetros = altura / 100;
+            decimal Pesoideal = peso / Alturaenmetros * Alturaenmetros;
+
+            if (Pesoideal < 20)
+            {
+                return -1;
+            }
+            else if (Pesoideal >= 20 && Pesoideal <= 25)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        
+        }
+        public bool MayordeEdad(int edad) 
+        {
+            int mayoria = edad;
+
+            if (mayoria > 18)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        public void validarsexo(char sexo)
+        {
+            char sexoValidado = char.ToUpper(sexo); // Convertir a mayúsculas
+
+            if (sexoValidado != 'H' && sexoValidado != 'M')
+            {
+                sexoValidado = 'H'; // Establecer como "H" si no es "H" ni "M"
+            }
+
+            return sexoValidado;
+        }
+
+        
+    }
+}
---
+++ TP2-Punto2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Punto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
